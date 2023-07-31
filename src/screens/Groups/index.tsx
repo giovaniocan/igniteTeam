9cac4dda341e91b0e1c395600b170bf3df747007
giovani <giovaniocan@gmail.com>
2023-07-31 15:17:50 -0300

    create ap and intaleed styled components

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { Container, Title } from './styles';
+
+export function Groups() {
+  return (
+    <Container>
+      <Title>Open up App.js to start working on your app!</Title>
+    </Container>
+  );
+}
+
