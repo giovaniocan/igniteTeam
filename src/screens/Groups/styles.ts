89e037eae2105191b0a92cd340ccf00802ef207d
giovani <giovaniocan@gmail.com>
2023-07-31 16:19:57 -0300

    fixed dtyled component's version

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -1,9 +1,8 @@
-import theme from "src/theme";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
-  background-color: ${({theme} => theme.)};
+  background-color: ${({theme}) => theme.COLORS.GRAY_700};
   align-items: center;
   justify-content: center;
 `;
