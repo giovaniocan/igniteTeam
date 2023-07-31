31eceaf4ec1e8d4cbf3589b1630421ca1ab0a460
giovani <giovaniocan@gmail.com>
2023-07-31 17:55:34 -0300

    created GroupCard component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,4 +1,5 @@
 
+import { GroupCard } from "@components/GroupCard";
 import { Container } from "./styles";
 import { Header } from "@components/Header";
 import { HighLight } from "@components/HighLight";
@@ -9,7 +10,10 @@ export function Groups() {
   return (
     <Container>
      <Header />
+
      <HighLight  title="Turmas" subtitle="Jogue com a sua turma " />
+
+     <GroupCard title="Turminha do Linkdin"/>
     </Container>
   );
 }
