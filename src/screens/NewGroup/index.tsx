ee816f601fb9fd85a5a8aae0fb52890e21efc9d6
giovani <giovaniocan@gmail.com>
2023-08-02 14:50:54 -0300

    it's able to pass the name of the group to the next page

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -4,12 +4,15 @@ import { HighLight } from "@components/HighLight";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
 
 export function NewGroup(){
+    const [group, setGroup] = useState('')
+
     const navigation = useNavigation();
 
     function handleNew(){
-        navigation.navigate('players', { group: 'Cria do Lúis' });
+        navigation.navigate('players', { group });
     }
 
     return(
@@ -26,6 +29,7 @@ export function NewGroup(){
 
                  <Input
                     placeholder="Nome da turma"
+                    onChangeText={setGroup}
                  />
 
                 <Button
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,26 +1,37 @@
+import { useRoute } from "@react-navigation/native";
+import { FlatList } from "react-native";
+import { useState } from "react";
+
 import { Header } from "@components/Header";
-import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 import { HighLight } from "@components/HighLight";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
-import { FlatList } from "react-native";
-import { useState } from "react";
 import { PlayerCard } from "@components/PlayerCard";
 import { ListEmpety } from "@components/ListEmpety";
 import { Button } from "@components/Button";
+
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
+
+type RouteParams = {
+    group: string;
+}
 
 export function Players(){
     const [team, setTeam] = useState('time a')
 
     const [players, setPlayers] = useState(['Giovani', 'João', 'Maca'])
 
+    const route = useRoute()
+    const { group } = route.params as RouteParams;
+
+
     return(
         <Container>
             <Header showBackButton />
 
             <HighLight
-                title="Nome da turma"
+                title={group}
                 subtitle="adicione a galera e separe os times"    
             />
 
