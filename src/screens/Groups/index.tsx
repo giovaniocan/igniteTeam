a8957429d471e12043dc3771b6b020e6a59ece17
giovani <giovaniocan@gmail.com>
2023-08-02 16:17:04 -0300

    showed the gorups in the initial screen

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,25 +1,47 @@
+import { useCallback, useEffect, useState } from "react";
+import { FlatList } from "react-native";
+import { useFocusEffect, useNavigation } from "@react-navigation/native"
 
-import { GroupCard } from "@components/GroupCard";
+import { groupsGetAll } from "@storage/group/groupGetAll";
+
 import { Container } from "./styles";
 import { Header } from "@components/Header";
 import { HighLight } from "@components/HighLight";
-import { useState } from "react";
-import { FlatList } from "react-native";
+import { GroupCard } from "@components/GroupCard";
 import { ListEmpety } from "@components/ListEmpety";
 import { Button } from "@components/Button";
-import { useNavigation } from "@react-navigation/native"
+
+
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([])
 
   const navigation = useNavigation()
 
-  
 
   function handleNewGroup() {
     navigation.navigate('new')
   
   }
+
+  async function fetchGroups() {
+    try {
+
+      const data = await groupsGetAll()
+
+      setGroups(data)
+      
+    } catch (error) {
+
+      console.log(error)
+
+    }
+  }
+
+useFocusEffect(useCallback(() => {
+  fetchGroups()
+}, []))
+
 
   return (
     <Container>
